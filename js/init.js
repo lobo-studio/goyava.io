0276fcc96f367a0a7c83f25aac192a88d35e140f
Feder <fedor.kokorin28@gmail.com>
2016-08-12 17:46:53 +0400

    project

---
+++ js/init.js
@@ -0,0 +1,25 @@
+$(function () {
+    // Transition a href
+    $('.gayava-menu a').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    $('.sign_in').click(function(e){
+        e.preventDefault();
+        $('#myModal').modal()
+        $('#page').addClass('add-blur')
+    });
+    $('.close').click(function () {
+        $('#page').removeClass('add-blur')
+    })
+});
+
